ca094c6bdc854f74863ca1e62e9d336c572cac1a
MinHieu2811 <cuunhatnhat51@gmail.com>
2023-02-10 01:21:07 +0700

    config sass

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../ui-component/shared/Navbar'
+import Footer from '../ui-component/shared/Footer'
+
+const Home = () => {
+  return (
+    <>
+        <Navbar />
+        <footer>
+            <Footer />
+        </footer>
+    </>
+  )
+}
+
+export default Home
---
+++ src/ui-component/shared/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+        <div className="container-fluid">
+          <div className="d-sm-flex justify-content-center justify-content-sm-between py-2 w-100">
+            <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright ©SE2-GROUP 7C20</span>
+          </div>
+        </div>
+      </footer>
+  )
+}
+
+export default Footer
