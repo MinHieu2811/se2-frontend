a754db7dd057d14cb86e5781f223e6c2eb1a73fd
MinHieu2811 <cuunhatnhat51@gmail.com>
2023-02-10 10:04:58 +0700

    add missing mixin sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router';
 import Home from './pages/Home';
