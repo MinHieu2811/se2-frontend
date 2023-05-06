126d4413d18fbc0efcb774bdd5c6bce1ed4b9d28
CODEjaVU <93817221+CODEjaVU@users.noreply.github.com>
2023-05-07 01:55:05 +0700

    admin discount feature

--- src/pages/admin/discount/CreateDiscount.tsx
+++ src/pages/admin/discount/CreateDiscount.tsx
@@ -1,116 +1,113 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Layout from "../../../ui-component/shared/Layout";
-import Helmet from "../../../ui-component/shared/Helmet";
 import { Form } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { axiosImageInstance, axiosInstance } from "../../../client-api";
 import { useToastContext } from "../../../ui-component/toast/ToastContext";
 import { REMOVE_ALL_AND_ADD } from "../../../ui-component/toast";
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import ToggleButton from 'react-bootstrap/ToggleButton';
-import DatePicker from "react-datepicker";
-
-import "react-datepicker/dist/react-datepicker.css";
-type Props = {};
+interface Props {}
 
 const CreateDiscount = (props: Props) => {
-  const [reviewImagesBlob, setReviewImagesBlob] = useState<File[]>([]);
+  const [date, setDate] = useState(new Date());
+  const [formatDate, setFormatDate] = useState("");
+  const [productID, setProductID] = useState("");
+  const [discountAmount, setDiscountPercentage] = useState("");
+
+
+  const applyFormattedDate = (date: Date) =>{
+    const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const formattedDate = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')} 00:00:00`;
+  setFormatDate(formattedDate);
+  }
+
   const { toastDispatch } = useToastContext();
-  const [checked, setChecked] = useState(false);
-  const [radioValue, setRadioValue] = useState('1');
-  const [startDate, setStartDate] = useState(new Date());
-  const radios = [
-    { name: 'All Products', value: '1' },
-    { name: 'Specific Category', value: '2' },
-    { name: 'Specific Product', value: '3' },
-  ];
-  const onImageChanged = (file: File[]) => {
-    if (file && file?.length) {
-      const arrayCheck = [".jpg", ".jpeg", ".png", "tiff", "webp", "gif"];
-      file.forEach((image) => {
-        const nameFile = image?.name;
-        if (!arrayCheck.some((v) => nameFile.includes(v))) {
-          toastDispatch({
-            type: REMOVE_ALL_AND_ADD,
-            payload: {
-              type: "is-danger",
-              content: `Please upload .jpg / .jpeg / .png / .tiff / .webp / .gif file only`,
-            },
-          });
-        } else {
-          setReviewImagesBlob([...reviewImagesBlob, image]);
-        }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(date);
+    try {
+       await axiosInstance
+      .post("https://se2-ecommerce.herokuapp.com/discount",{
+        productId: productID,
+        discountAmount: discountAmount,
+        expiryDate: formatDate,
+      }).catch((err) => {
+        console.log(err);
+        toastDispatch({
+          type: REMOVE_ALL_AND_ADD,
+          payload: {
+            type: "is-danger",
+            content: "Something went wrong!",
+          },
+        });
       });
+
+
+      // clear the form fields after submission
+      setProductID("");
+      setDiscountPercentage("");
+      setDate(new Date());
+    } catch (error) {
+      console.error(error);
     }
   };
 
-  const handleChange = () => {};
+  const handleDiscountNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setProductID(event.target.value);
+  };
+
+  const handleDiscountPercentageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDiscountPercentage(event.target.value);
+  };
+
   return (
     <Layout>
       <>
-        <Helmet title="Create New Discount" />
         <div className="card w-100">
           <div className="card-body ">
-            <form>
+            <form onSubmit={handleSubmit}>
               <Form.Group className="mb-4 width-all">
-                <label htmlFor="DiscountName">Discount Name</label>
+                <label htmlFor="DiscountID">Product ID</label>
                 <Form.Control
                   type="text"
-                  name="DiscountName"
+                  name="DiscountID"
                   className="form-control"
-                  id="DiscountName"
-                  placeholder="Discount Name"
-                  onChange={handleChange}
+                  id="DiscountiD"
+                  placeholder="Discount ID"
+                  value={productID}
+                  onChange={handleDiscountNameChange}
                 />
               </Form.Group>
+
               <Form.Group className="mb-4 width-all">
-                <label htmlFor="DiscountCode">Discount Code</label>
+                <label htmlFor="DiscountAmount">Discount Amount</label>
                 <Form.Control
                   type="text"
-                  name="DiscountCode"
+                  name="DiscountAmount"
                   className="form-control"
-                  id="DiscountCode"
-                  placeholder="Discount Code"
-                  onChange={handleChange}
-                />
-                {/* <Button variant="primary">Generate Code</Button> */}
-                
-                <button className="btn btn-primary mt-2">Generate Code</button>
-              </Form.Group>
-              <Form.Group className="mb-4 width-all">
-                <label htmlFor="DiscountDesc">Discount Description</label>
-                <textarea className="form-control text-area mr-2" onChange={handleChange} placeholder="Discount Description" style={{width: "100%", resize: "vertical"}} name="DiscountDesc" id="DiscountDesc" />
-              </Form.Group>
-              <Form.Group className="mb-4 width-all">
-                <label htmlFor="DiscountPercentage">Discount Percentage</label>
-                <Form.Control
-                  type="text"
-                  name="DiscountPercentage"
-                  className="form-control"
-                  id="DiscountPercentage"
-                  placeholder="DiscountPercentage"
-                  onChange={handleChange}
+                  id="DiscountAmount"
+                  placeholder="Discount Amount"
+                  value={discountAmount}
+                  onChange={handleDiscountPercentageChange}
                 />
               </Form.Group>
-              <Form.Group className="mb-4 width-all">
-                <ButtonGroup>
-                    {radios.map((radio, idx) => (
-                    <ToggleButton
-                        key={idx}
-                        id={`radio-${idx}`}
-                        type="radio"
-                        variant={idx % 2 ? 'outline-success' : 'outline-danger'}
-                        name="radio"
-                        value={radio.value}
-                        checked={radioValue === radio.value}
-                        onChange={(e) => setRadioValue(e.currentTarget.value)}
-                    >
-                        {radio.name}
-                    </ToggleButton>
-                    ))}
-                </ButtonGroup>         
-              </Form.Group>
-              <label>Expiry Date </label>
-              <DatePicker selected={startDate} onChange={date => date && setStartDate(date)} />
-              <button className="btn btn-primary mr-1 mt-2">Submit</button>
+
+              <label>Expiry Date</label>
+              <br />
+              <DatePicker selected={date} onChange={(date) => {
+                if (date) {
+                  setDate(date);
+                  applyFormattedDate(date);
+                }
+                }} />
+              <br />
+              <br />
+              <button className="btn btn-primary mr-1 mt-2" type="submit">
+                Submit
+              </button>
             </form>
           </div>
         </div>
